5cbc5c7fecd6ff7fe3a2e875f5fea701129b68cf
Charlie Hanacek <charlie@czhanacek.com>
2018-10-28 17:32:30 -0700

    should work?

---
+++ sender.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+import os
+import re
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected")
+
+def main():
+    client = mqtt.Client("laptop")
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    hostname = os.environ["MQTT_SERVER"]
+
+    client.connect(hostname, 1883, 60)
+
+    client.subscribe("ssh_connections", 0)
+    logfile = open("/var/log/auth.log", "r")
+    while(1):
+        where = logfile.tell()
+        line = logfile.readline()
+        if not line:
+            logfile.seek(where)
+        else:
+            if(re.match(".+pam_unix(sshd:auth): authentication failure.+", line)):
+                client.publish("ssh_connections", "connection")
+        client.loop()
+
+if __name__ == "__main__":
+    main()
+
